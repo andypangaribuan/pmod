fc7cad31bc3939f6afce8fae9bd51b18a3510b39
Andy Pangaribuan <iam.pangaribuan@gmail.com>
2024-10-21 17:21:50 +0700

    update

--- pmod/script_server.py
+++ pmod/script_server.py
@@ -347,8 +347,8 @@ class ScripServer:
                     if err_message is None:
                         _, nano_value = self.__util.get_last_index_version(input_version)
                         _, prefer_nano_value = self.__util.get_last_index_version(self.__prefer_next_version)
-                        if nano_value < prefer_nano_value:
-                            err_message = f'🔴 nano version "{nano_value}" cannot less than prefer next version "{prefer_nano_value}"'
+                        if nano_value <= prefer_nano_value:
+                            err_message = f'🔴 nano version "{nano_value}" cannot less or same than prefer next version "{prefer_nano_value}"'
 
                 case 'rc':
                     if len(input_version.release) != 3 or input_version.pre is None:
@@ -359,14 +359,17 @@ class ScripServer:
                     if err_message is None and input_version.pre[0] != 'rc':
                         err_message = '🔴 mush have "rc" part'
 
-                    if err_message is None and input_version.pre[1] < self.__prefer_next_version.pre[1]:
-                        err_message = f'🔴 rc version "{input_version.pre[1]}" cannot less than prefer next version "{self.__prefer_next_version.pre[1]}"'
+                    if err_message is None and input_version.pre[1] <= self.__prefer_next_version.pre[1]:
+                        err_message = f'🔴 rc version "{input_version.pre[1]}" cannot less or same than prefer next version "{self.__prefer_next_version.pre[1]}"'
 
                 case 'prod':
                     if len(input_version.release) != 3 or f'{input_version.major}.{input_version.minor}.{input_version.micro}' != input_version.public:
                         err_message = '🔴 version on prod must be using 3 part number'
 
                     validate_major_minor_micro()
+
+                    if err_message is None and input_version.micro <= self.__prefer_next_version.micro:
+                        err_message = f'🔴 micro version "{input_version.micro}" cannot less or same than prefer next version "{self.__prefer_next_version.micro}"'
 
             if err_message is not None:
                 print(err_message)
